330d1a99e5510b974391ad4908f95cdefd81e76a
srimani <srimanikantaangadi@gmail.com>
2025-06-01 22:37:14 +0000

    changes are done

---
+++ shell@scripting/07-arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MOVIES=("pushpa" "rrr" "chandini")
+#index starts from 0 & size is 3
+
+
+echo "first movie : ${MOVIES[0]}"
+echo "second movie : ${MOVIES[1]}"
+echo "third movie : ${MOVIES[2]}"
+
+echo "All movies list is here : ${MOVIES[@]}"
